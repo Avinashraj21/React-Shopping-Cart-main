5a9bf50e71efcfa980e30294847df57ec0fedf87
Avinashraj21 <115334230+Avinashraj21@users.noreply.github.com>
2024-09-16 12:17:06 +0530

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='py-3 bg-secondary d-flex justify-content-center'>
-        <p className='fw-bolder text-white'>&copy; Code With Yousaf</p>
+        <p className='fw-bolder text-white'>visite again</p>
     </div>
   )
 }
